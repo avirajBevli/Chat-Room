3e9726d6edf85d68c9b3e816de8ad0835395992a
Aviraj Bevli <45387375+avirajBevli@users.noreply.github.com>
2023-06-21 01:18:04 +0530

    Update myclient.cpp
    made print OK

--- myclient.cpp
+++ myclient.cpp
@@ -2,10 +2,15 @@
 
 int main(){
 	client_init();
-	thread t_temp(recv_messages);
-	t_recv_messages = move(t_temp);	
+	thread t_temp_recv(recv_messages);
+	thread t_temp_send(send_messages);
+	t_recv_messages = move(t_temp_recv);
+	t_send_messages = move(t_temp_send);
 	if(t_recv_messages.joinable()){
 		t_recv_messages.join();
+	}
+	if(t_send_messages.joinable()){
+		t_send_messages.join();
 	}
 	return 0;
 }
